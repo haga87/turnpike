1c417e0b1216f66c876f3920e3412697a3fb5351
Eduardo Fernández <fernandez14@outlook.com>
2014-09-25 17:48:12 -0500

    Update server.go

--- server.go
+++ server.go
@@ -5,6 +5,7 @@
 package turnpike
 
 import (
+	"bufio"
 	"code.google.com/p/go.net/websocket"
 	"encoding/json"
 	"fmt"
@@ -26,6 +27,47 @@ const (
 	clientConnTimeout = 6
 	clientMaxFailures = 3
 )
+
+
+func newServerConn(rwc io.ReadWriteCloser, buf *bufio.ReadWriter, req *http.Request, config *Config, handshake func(*Config, *http.Request) error) (conn *Conn, err error) {
+        var hs serverHandshaker = &hybiServerHandshaker{Config: config}
+        code, err := hs.ReadHandshake(buf.Reader, req)
+        if err == ErrBadWebSocketVersion {
+                fmt.Fprintf(buf, "HTTP/1.1 %03d %s\r\n", code, http.StatusText(code))
+                fmt.Fprintf(buf, "Sec-WebSocket-Version: %s\r\n", SupportedProtocolVersion)
+                buf.WriteString("\r\n")
+                buf.WriteString(err.Error())
+                buf.Flush()
+                return
+        }
+        if err != nil {
+                fmt.Fprintf(buf, "HTTP/1.1 %03d %s\r\n", code, http.StatusText(code))
+                buf.WriteString("\r\n")
+                buf.WriteString(err.Error())
+                buf.Flush()
+                return
+        }
+        if handshake != nil {
+                err = handshake(config, req)
+                if err != nil {
+                        code = http.StatusForbidden
+                        fmt.Fprintf(buf, "HTTP/1.1 %03d %s\r\n", code, http.StatusText(code))
+                        buf.WriteString("\r\n")
+                        buf.Flush()
+                        return
+                }
+        }
+        err = hs.AcceptHandshake(buf.Writer)
+        if err != nil {
+                code = http.StatusBadRequest
+                fmt.Fprintf(buf, "HTTP/1.1 %03d %s\r\n", code, http.StatusText(code))
+                buf.WriteString("\r\n")
+                buf.Flush()
+                return
+        }
+        conn = hs.NewServerConn(buf, rwc, req)
+        return
+}
 
 // Server represents a server of a WebSocket.
 type SocketServer struct {
@@ -56,7 +98,7 @@ func (s SocketServer) serveWebSocket(w http.ResponseWriter, req *http.Request) {
         // the client did not send a handshake that matches with protocol
         // specification.
         defer rwc.Close()
-        conn, err := *websocket.newServerConn(rwc, buf, req, &s.Config, s.Handshake)  
+        conn, err := newServerConn(rwc, buf, req, &s.Config, s.Handshake)  
 
         if conn == nil {
                 panic("unexpected nil conn")
